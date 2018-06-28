56df41592c8167963f8fd09e82dc32b942dfb94e
lzg15934250144 <876503424@qq.com>
2018-06-29 07:43:38 +0800

    sunmit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+	let lbt=document.querySelector(".lunbo");
+	console.log(lbt);
+	let lbd=document.querySelector(".lunbodian");
+	console.log(lbd);
+	let boxwidth=document.
+}
